c112c43cc3fb7d8a3a9fa672190822343371bb36
Matvey Matvienko <138231947+Traffaello@users.noreply.github.com>
2024-11-22 22:59:00 +0300

    Update square.py

--- square.py
+++ square.py
@@ -2,8 +2,6 @@ def area(a):
     """Функция вычисляет площадь квадрата со стороной a"""
     if a < 0:
         raise ValueError("Negative number in side")
-    if type(a) != int:
-        raise TypeError("Incorrect type of side")
     return a * a
 
 
@@ -11,6 +9,4 @@ def perimeter(a):
     """Функция вычисляет периметр квадрата со стороной a"""
     if a < 0:
         raise ValueError("Negative number in side")
-    if type(a) != int:
-        raise TypeError("Incorrect type of side")
     return 4 * a
