2bd92d746bfac7ef893c8fd3276f29791c58c5ce
Matvey Matvienko <138231947+Traffaello@users.noreply.github.com>
2024-11-22 22:58:38 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,8 +5,6 @@ def area(r):
     """Функция вычисляет значение площади круга с радиусом r"""
     if r < 0:
         raise ValueError("Negative number in radius")
-    if type(r) != int:
-        raise TypeError("Incorrect type of radius")
     return math.pi * r * r
 
 
@@ -14,6 +12,4 @@ def perimeter(r):
     """Функция вычисляет значение периметра круга с радиусом r"""
     if r < 0:
         raise ValueError("Negative number in radius")
-    if type(r) != int:
-        raise TypeError("Incorrect type of radius")
     return 2 * math.pi * r
