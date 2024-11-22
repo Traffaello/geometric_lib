47bdfebb4a3a13b4ca1e7477c65c8afde32be354
Matvey Matvienko <138231947+Traffaello@users.noreply.github.com>
2024-11-22 23:07:22 +0300

    Update test_triangle.py

--- unit-tests/test_triangle.py
+++ unit-tests/test_triangle.py
@@ -1,22 +1,22 @@
 import unittest
 import sys
-from triangle import area, perimetr
+from triangle import area, perimeter
 
 sys.path.append("..")
 
 
 class TestCaseTriangle(unittest.TestCase):
-    def perimetr_standart_1(self):
+    def perimeter_standart_1(self):
         self.assertEqual(perimeter(5, 5, 5), 15)
 
-    def perimetr_standart_2(self):
+    def perimeter_standart_2(self):
         self.assertEqual(perimeter(0, 5, 5), 0)
 
-    def perimetr_standart_3(self):
-        self.assertEqual(perimetr(5, 0, 5), 0)
+    def perimeter_standart_3(self):
+        self.assertEqual(perimeter(5, 0, 5), 0)
 
-    def perimetr_standart_3(self):
-        self.assertEqual(perimetr(5, 5, 0), 0)
+    def perimeter_standart_3(self):
+        self.assertEqual(perimeter(5, 5, 0), 0)
 
     def area_standart_1(self):
         self.assertEqual(area(5, 4), 10)
@@ -27,7 +27,7 @@ class TestCaseTriangle(unittest.TestCase):
     def area_standart_3(self):
         self.assertEqual(area(0, 5), 0)
 
-    def perimetr_invalid(self):
+    def perimeter_invalid(self):
         with self.assertRaises(TypeError):
             perimeter("five", "five", "five")
         with self.assertRaises(ValueError):
