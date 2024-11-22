c16627341422c64d4ff10b889e0fb91d096ee111
Matvey Matvienko <138231947+Traffaello@users.noreply.github.com>
2024-11-22 22:19:28 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,8 +1,20 @@
-def area(a, b, c):
-    ''' Функция вычисляет значение площади треугольника со сторонами a, b, c'''
-    return (a + b + c) / 2
+def area(a, h):
+    ''' Функция вычисляет значение площади треугольника со стороной a и высотой h'''
+    if a<0 or h<0:
+        raise ValueError("Negative number")
+    if type (a)!=int or type (h)!=int:
+        raise TypeError("Incorrect type")
+    return (a*h)/2
 
 
 def perimeter(a, b, c):
     ''' Функция вычисляет значение периметра треугольника со сторонами a, b, c '''
+    if a<0 or b<0 or c<0:
+        raise ValueError("Negative number in radius")
+    if a==0 or b==0 or c==0:
+        return 0
+    if type (a)!=int or type (b)!=int or type (c)!=int:
+        raise TypeError("Incorrect type of radius")
+    if a+b<c or a+c<b or b+c<a:
+        raise ValueError ("Not a triangle")
     return a + b + c
